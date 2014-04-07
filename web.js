59e816f0800a9caaac72cfa579b4f2343f70547d
Aankit Patel <Aankit@Aankits-MacBook-Pro.local>
2014-04-07 13:14:14 -0400

    basic static pages

--- web.js
+++ web.js
@@ -5,8 +5,20 @@ var app = express();
 
 app.use(logfmt.requestLogger());
 
+app.get('/data', function(req, res){
+	res.send('<h1> Data Tables</h1>')
+})
+
+app.get('/charts', function(req, res){
+	res.send('Charts live here, links to data tables')
+})
+
+app.get('/poetry', function(req, res){
+	res.send('plan to run a python script to generate poetry written based on data from the panel')
+})
+
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  res.send('Here I will introduce the solar panel briefly and have links to data/get involved');
 });
 
 var port = Number(process.env.PORT || 5000);
